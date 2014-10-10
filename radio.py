a2b31b1c22a474d5451314fed10a40c8713d26f0
Silviu Vulcan <git@silviuvulcan.ro>
2014-10-10 18:12:43 +0000

    removed debugging prints

--- radio.py
+++ radio.py
@@ -18,22 +18,16 @@ maxstat = len(statarr)-1 # reads whin one extra
 stat = 0
 
 # play something on start
-print stat
 print statarr[stat]
 command = 'mplayer -really-quiet -slave -playlist '+statarr[stat]
 mp = subprocess.Popen(command.split(),stdin=PIPE,stderr=subprocess.STDOUT, stdout=PIPE)
 a = mp.poll()
-print(a)
 
 while True:
-    a = mp.poll()
-    print(a)
-    if GPIO.input(23)==1:
+    if GPIO.input(23)==1 and GPIO.input(24)==0:
 	stat-=1
 	if stat == -1 :
-		print stat
 		stat=maxstat-1 			# empty line on last line
-	print stat
 	print statarr[stat]
 	if mp.poll() is None: 			# check if the process didn't crash in the mean time
 	     mp.stdin.write('quit \n')
@@ -41,17 +35,20 @@ while True:
 	mp = subprocess.Popen(command.split(),stdin=PIPE,stderr=subprocess.STDOUT, stdout=PIPE)
 	sleep(0.5) # ignore long press
 
-    if GPIO.input(24)==1:
+    if GPIO.input(24)==1 and GPIO.input(23)==0:
 	stat+=1
 	if stat == maxstat :
-		print stat
 		stat=0
-	print stat
 	print statarr[stat]
 	if mp.poll() is None:
 	     mp.stdin.write('quit \n')
 	command = 'mplayer -really-quiet -slave -playlist '+statarr[stat]
 	mp = subprocess.Popen(command.split(),stdin=PIPE,stderr=subprocess.STDOUT, stdout=PIPE)
 	sleep(0.5) # ugly way to ignore longer presses
+    if GPIO.input(24)==1 and GPIO.input(23)==1:
+	sleep(1)
+	if GPIO.input(24)==1 and GPIO.input(23)==1:
+	    os.system("poweroff")
+	    raise SystemExit(0)
     sleep(0.1);
 GPIO.cleanup()
